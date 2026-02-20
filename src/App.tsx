285cb76fd66cce8159321fb0cac8123d905ebd6c
s1yle <1373003655@qq.com>
2026-02-20 21:10:10 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ import {
 } from './pages';
 
 // MC风格背景图URL
-// const BACKGROUND_IMAGE_URL = './assets/img/bg-1.png';
-const BACKGROUND_IMAGE_URL = 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+const BACKGROUND_IMAGE_URL = './src/assets/img/bg-1.png';
+// const BACKGROUND_IMAGE_URL = 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 // 组件映射
 const componentMap: Record<string, React.FC> = {
